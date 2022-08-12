022e584e5d37dec23a0433bf5d24c3bf2db6c16d
Yevhen <eug.z2004@gmail.com>
2022-08-12 12:41:22 +0300

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+import { getRandomHexColor } from './random-color-generator';
 
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+const DELAY = 1000;
+let intervalId = null;
+
+const changeColor = () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+};
+
+const onStartBtnCLick = e => {
+  e.currentTarget.setAttribute('disabled', '');
+  refs.stopBtn.removeAttribute('disabled');
+  intervalId = setInterval(changeColor, DELAY);
+};
+
+const onStopBtnCLick = e => {
+  e.currentTarget.setAttribute('disabled', '');
+  refs.startBtn.removeAttribute('disabled');
+  clearInterval(intervalId);
+};
+
+refs.startBtn.addEventListener('click', onStartBtnCLick);
+refs.stopBtn.addEventListener('click', onStopBtnCLick);
+
+refs.stopBtn.setAttribute('disabled', '');
