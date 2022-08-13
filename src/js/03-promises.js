2de0ca718e93344f0c59269b2d4c57a7ca1bfe58
Yevhen <eug.z2004@gmail.com>
2022-08-14 01:35:31 +0300

    task3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
+import { Notify } from 'notiflix';
+
+const formRef = document.querySelector('form.form');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function showSuccessMessage({ position, delay }) {
+  Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+}
+
+function showErrorMessage({ position, delay }) {
+  Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const {
+    delay: { value: delay },
+    step: { value: step },
+    amount: { value: amount },
+  } = e.currentTarget.elements;
+  for (let i = 0; i < Number(amount); i += 1) {
+    createPromise(i + 1, Number(delay) + i * Number(step))
+      .then(showSuccessMessage)
+      .catch(showErrorMessage);
   }
 }
+formRef.addEventListener('submit', onFormSubmit);
